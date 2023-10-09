9ff33acad589f494e91534e47f4a8434c948a44d
Álvaro Francis de Moura <40585050+brutusdev@users.noreply.github.com>
2023-10-09 19:20:33 -0300

    first commit

---
+++ src/main/java/br/com/alvaromoura/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,10 @@
+package br.com.alvaromoura.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class MinhaPrimeiraController {
+    
+}
