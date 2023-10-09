a67527a6e0a7cd33dbe274efc2707d4b1cc47d11
Álvaro Francis de Moura <40585050+brutusdev@users.noreply.github.com>
2023-10-09 19:39:44 -0300

    update diretórios

--- src/main/java/br/com/alvaromoura/todolist/controller/MinhaPrimeiraController.java
+++ src/main/java/br/com/alvaromoura/todolist/controller/MinhaPrimeiraController.java
@@ -1,10 +1,15 @@
 package br.com.alvaromoura.todolist.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
+@RequestMapping("/primeiraRota")
 public class MinhaPrimeiraController {
     
+    @GetMapping("/")
+    public String PrimeiraMEnsagem() {
+        return "Funcionou";
+    }
 }
